79d155e9275ce4e3306b6d04e9002c3d86459a6d
Examinati <72595444+Eximinati@users.noreply.github.com>
2023-03-04 00:03:10 +0500

    Work

--- Organs/commands/Econ/work.js
+++ Organs/commands/Econ/work.js
@@ -1,6 +1,5 @@
 const economyJs = require('../../../models/economic')
 const Group = require("../../../models/group")
-const moment = require('moment-timezone')
 
 module.exports = {
   name: 'work',
@@ -8,29 +7,23 @@ module.exports = {
   usage:`${prefa}work`,
   desc:"Work to earn coins.",
   category:"Economy",
+  cool:28800,
   react:"✅",
-  start:async(client,m,{command,prefix,pushname,pushName,args})=>{
-    if (!m.from.endsWith("@g.us")) {
-      return m.reply("Please use this command in a group.");
-    }
+    start:async(client,m,{command,prefix,pushname , pushName,args})=>{
 
-    const groupId = m.from;
+      if (!m.from.endsWith("@g.us")) {
+        return m.reply("Please use this command in a group.");
+      }
+  
+      const groupId = m.from;
+  
+      
+        const group = await Group.findOne({ groupId });
+        if (!group || !group.enabled) {
+          return m.reply("Economy is not enabled in this group. Type '.support' to see casino group link ");
+        }
 
-    const group = await Group.findOne({ groupId });
-    if (!group || !group.enabled) {
-      return m.reply("Economy is not enabled in this group. Type '.support' to see casino group link ");
-    }
-
-    const cooldownHours = 8;
-    const lastWork = moment.tz(economy.lastWork, 'Asia/Kolkata'); // Replace 'Asia/Kolkata' with your desired timezone
-    const currentTime = moment.tz('Asia/Kolkata'); // Replace 'Asia/Kolkata' with your desired timezone
-
-    if (lastWork && lastWork.add(cooldownHours, 'hours').isAfter(currentTime)) {
-      const remainingCooldown = lastWork.add(cooldownHours, 'hours').diff(currentTime, 'minutes');
-      return m.reply(`You can't work again yet. Please wait ${remainingCooldown} minutes.`);
-    }
-
-    const minCoins = 100;
+        const minCoins = 100;
     const maxCoins = 1000;
     const earnedCoins = Math.floor(Math.random() * (maxCoins - minCoins + 1)) + minCoins;
 
@@ -38,9 +31,10 @@ module.exports = {
     if (!economy) return message.channel.send('You don\'t have an economy profile.');
 
     economy.wallet += earnedCoins;
-    economy.lastWork = currentTime.toDate();
     await economy.save();
 
     m.reply(`You worked hard and earned ${earnedCoins} coins!`);
-  }
+  
+      
+    }
 }
