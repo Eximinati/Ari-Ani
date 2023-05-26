85a2b94de107463fa3b788dc0f1362f614fcbe3c
YujiMODS <123268555+YujiMODS@users.noreply.github.com>
2023-05-26 15:44:15 +0200

    Update mods.js

--- src/commands/General/mods.js
+++ src/commands/General/mods.js
@@ -6,17 +6,17 @@ module.exports={
     usage:`${prefa}mods`,
     desc:"shows mods list",
     category:"General",
-    react:"✅",
+    react:"🏅",
 
     start:async(client,m,{yaOwn})=>{
         const mod= yaOwn
         let mods=`
-        *🎉 Ari-Ani Mods 🎉*\n`
+        *🏮 Galaxia Mods 🏮*\n`
         for(let i=0;i<mod.length;i++){
 
             const um= await client.username(mod[i])
             //const um= await client.username(mod[i]+'@s.whatsapp.net')
-            mods+=`\n✨${i+1}\n*👤 Name:* ${um}\n*📱 Contact:* http://wa.me/+${mod[i].split("@")[0]}\n`
+            mods+=`\n✨${i+1}\n*🎗 Name:* ${um}\n* 🧧Contact:* http://wa.me/+${mod[i].split("@")[0]}\n`
         }
         await  client.sendMessage(m.from,{image:{url:'https://images2.alphacoders.com/104/1044538.png'},caption:mods},{quoted:m})  
 
